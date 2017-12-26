9d634b49aa506afb720f184ac86389562b1c66b8
Adem <adem.karabulut@nevalabs.com>
2017-12-26 17:47:41 +0300

    Sensör listesindeki butonların yanına checkbox ikonu konuldu. Sensör listesi ekranına kayıt butonu eklendi.

--- app/src/main/java/org/ytu/adem/datacollector/SensorListActivity.java
+++ app/src/main/java/org/ytu/adem/datacollector/SensorListActivity.java
@@ -6,11 +6,15 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import org.ytu.adem.datacollector.sensors.acceleration.AccelerationActivity;
 import org.ytu.adem.datacollector.sensors.accelerometer.AccelerometerActivity;
 import org.ytu.adem.datacollector.sensors.gravity.GravityActivity;
+import org.ytu.adem.datacollector.sensors.gyroscope.GyroscopeActivity;
 import org.ytu.adem.datacollector.sensors.humidity.HumidityActivity;
 import org.ytu.adem.datacollector.sensors.light.LightActivity;
 import org.ytu.adem.datacollector.sensors.magneticField.MagneticFieldActivity;
@@ -19,7 +23,7 @@ import org.ytu.adem.datacollector.sensors.proximity.ProximityActivity;
 import org.ytu.adem.datacollector.sensors.rotationVector.RotationVectorActivity;
 import org.ytu.adem.datacollector.sensors.temperature.TemperatureActivity;
 
-public class SensorListActivity extends AppCompatActivity  {
+public class SensorListActivity extends AppCompatActivity {
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
     private Sensor temperatureSensor;
@@ -57,26 +61,38 @@ public class SensorListActivity extends AppCompatActivity  {
     }
 
     private void setButtonStates() {
-        isSensorButtonEnabled(accelerometerSensor, (Button) findViewById(R.id.btn_accelerometer));
-        isSensorButtonEnabled(temperatureSensor, (Button) findViewById(R.id.btn_temperature));
-        isSensorButtonEnabled(gravitySensor, (Button) findViewById(R.id.btn_gravity));
-        isSensorButtonEnabled(gyroscopeSensor, (Button) findViewById(R.id.btn_gyroscope));
-        isSensorButtonEnabled(lightSensor, (Button) findViewById(R.id.btn_light));
-        isSensorButtonEnabled(accelerationSensor, (Button) findViewById(R.id.btn_linear_acceleration));
-        isSensorButtonEnabled(magneticFieldSensor, (Button) findViewById(R.id.btn_magnetic_field));
-        isSensorButtonEnabled(pressureSensor, (Button) findViewById(R.id.btn_pressure));
-        isSensorButtonEnabled(proximitySensor, (Button) findViewById(R.id.btn_proximity));
-        isSensorButtonEnabled(humiditySensor, (Button) findViewById(R.id.btn_relative_humidity));
-        isSensorButtonEnabled(rotationVectorSensor, (Button) findViewById(R.id.btn_rotation_vector));
+        hideSensorCheckboxAndButton(accelerometerSensor, (Button) findViewById(R.id.btn_accelerometer), (CheckBox) findViewById(R.id.check_accelerometer));
+        hideSensorCheckboxAndButton(temperatureSensor, (Button) findViewById(R.id.btn_temperature), (CheckBox) findViewById(R.id.check_temperature));
+        hideSensorCheckboxAndButton(gravitySensor, (Button) findViewById(R.id.btn_gravity), (CheckBox) findViewById(R.id.check_gravity));
+        hideSensorCheckboxAndButton(gyroscopeSensor, (Button) findViewById(R.id.btn_gyroscope), (CheckBox) findViewById(R.id.check_gyroscope));
+        hideSensorCheckboxAndButton(lightSensor, (Button) findViewById(R.id.btn_light), (CheckBox) findViewById(R.id.check_light));
+        hideSensorCheckboxAndButton(accelerationSensor, (Button) findViewById(R.id.btn_linear_acceleration), (CheckBox) findViewById(R.id.check_linear_acceleration));
+        hideSensorCheckboxAndButton(magneticFieldSensor, (Button) findViewById(R.id.btn_magnetic_field), (CheckBox) findViewById(R.id.check_magnetic_field));
+        hideSensorCheckboxAndButton(pressureSensor, (Button) findViewById(R.id.btn_pressure), (CheckBox) findViewById(R.id.check_pressure));
+        hideSensorCheckboxAndButton(proximitySensor, (Button) findViewById(R.id.btn_proximity), (CheckBox) findViewById(R.id.check_proximity));
+        hideSensorCheckboxAndButton(humiditySensor, (Button) findViewById(R.id.btn_relative_humidity), (CheckBox) findViewById(R.id.check_relative_humidity));
+        hideSensorCheckboxAndButton(rotationVectorSensor, (Button) findViewById(R.id.btn_rotation_vector), (CheckBox) findViewById(R.id.check_rotation_vector));
     }
 
-    private void isSensorButtonEnabled(Sensor sensor, Button button) {
-        if(sensor == null) button.setVisibility(View.GONE);
+    private void hideSensorCheckboxAndButton(Sensor sensor, Button button, CheckBox checkbox) {
+        if (!isSensorAvailable(sensor)) {
+            button.setVisibility(View.GONE);
+            checkbox.setVisibility(View.GONE);
+        }
+    }
+
+    private boolean isSensorAvailable(Sensor sensor) {
+        if (sensor == null) return false;
+        return true;
     }
 
     public void openAccelerometerActivity(View view) {
         Intent accelerometerIntent = new Intent(this, AccelerometerActivity.class);
         startActivity(accelerometerIntent);
+    }
+
+    public void clickAccelerometerCheck(View view) {
+        CheckBox checkbox = (CheckBox) view.findViewById(R.id.check_accelerometer);
     }
 
     public void openTemperatureActivity(View view) {
@@ -87,6 +103,11 @@ public class SensorListActivity extends AppCompatActivity  {
     public void openGravityActivity(View view) {
         Intent gravityIntent = new Intent(this, GravityActivity.class);
         startActivity(gravityIntent);
+    }
+
+    public void openGyroscopeActivity(View view) {
+        Intent gyroscopeIntent = new Intent(this, GyroscopeActivity.class);
+        startActivity(gyroscopeIntent);
     }
 
     public void openAccelerationActivity(View view) {
---
+++ app/src/main/java/org/ytu/adem/datacollector/sensors/gyroscope/GyroscopeActivity.java
@@ -0,0 +1,8 @@
+package org.ytu.adem.datacollector.sensors.gyroscope;
+
+/**
+ * Created by adem on 26.12.2017.
+ */
+
+public class GyroscopeActivity {
+}
